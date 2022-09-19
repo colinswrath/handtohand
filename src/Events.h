f45e0bd52da2c49813d3396d0e525b54dd550847
colinswrath <colinkelley30@gmail.com>
2022-09-18 22:12:37 -0400

    Fixed beast race check

--- src/Events.h
+++ src/Events.h
@@ -16,7 +16,6 @@ namespace Events
 		RE::BSEventNotifyControl ProcessEvent(const RE::TESHitEvent* a_event, RE::BSTEventSource<RE::TESHitEvent>* a_eventSource) override
 		{
 			using HitFlag = RE::TESHitEvent::Flag;
-
 			if (!a_event || !a_eventSource) {
 				logger::error("Event Source Not Found!");
 				return RE::BSEventNotifyControl::kContinue;
@@ -24,23 +23,22 @@ namespace Events
 
 			if (!a_event->flags.any(HitFlag::kBashAttack) && a_event->target)
 			{
-
 				if (!a_event || !a_event->cause || !a_event->cause->IsPlayerRef() || a_event->target->IsNot(RE::FormType::ActorCharacter) || !a_event->source)
-					return RE::BSEventNotifyControl::kContinue;				
+					return RE::BSEventNotifyControl::kContinue;		
 
 				auto defender = a_event->target->As<RE::Actor>();
 				auto attackingWeapon = RE::TESForm::LookupByID<RE::TESObjectWEAP>(a_event->source);
 
 				if (!defender || !attackingWeapon || !defender->currentProcess || !defender->currentProcess->high || !attackingWeapon->IsMelee() || !defender->Get3D())
 					return RE::BSEventNotifyControl::kContinue;
-				
+
 				auto player = a_event->cause->As<RE::Actor>();
 				auto playerAttkData = player->currentProcess->high->attackData;
 
 				if (!playerAttkData)
 					return RE::BSEventNotifyControl::kContinue;
 
-				if ((defender->ActorState::GetLifeState() != RE::ACTOR_LIFE_STATE::kDead) && IsBeastRace() && attackingWeapon->IsHandToHandMelee())
+				if ((defender->ActorState::GetLifeState() != RE::ACTOR_LIFE_STATE::kDead) && !IsBeastRace() && attackingWeapon->IsHandToHandMelee())
 				{
 					ApplyHandToHandXP();
 				}
